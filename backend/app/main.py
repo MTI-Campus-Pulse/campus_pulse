6794c3147bee63531ef239af92b311f6b6e935b0
ViolaMaged <163117384+ViolaMaged@users.noreply.github.com>
2026-02-14 16:35:27 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from routes.query_router import router as query_router
 from app.routes import router as approval_router
 
-
 app = FastAPI(title="Campus Pulse")
 
 app.include_router(query_router)
