a5aaec1843b7578892048e75224e53313e9c8f21
Mazen Essam <mazenessam713@gmail.com>
2026-02-14 21:31:42 +0200

    Update user model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,6 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# def read_root():
-#     return {"message": "Campus Pulse Backend is running!"}  # اختبار بسيط
-
-
-
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routes import auth, posts, feedback, chat
-from fastapi import FastAPI
 from .database import fake_db, posts_db, feedback_db
 from backend.app.services.campus_pipeline import process_text
 from pydantic import BaseModel
